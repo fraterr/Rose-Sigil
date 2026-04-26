ef60b16a50dd9a0670358303000aa5ea1e857d92
fraterr <practicalhumanism@gmail.com>
2026-04-26 17:32:25 +0200

    Branding: Add custom minimalist esoteric logo to the app

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ function App() {
     <div className="app-wrapper">
       <header className="main-header">
         <div className="logo">
-          <Sparkles size={24} className="logo-icon" />
+          <img src="/Rose-Sigil/logo.png" alt="Rose Sigil Logo" className="logo-img" />
           <h1>Rose Sigil</h1>
         </div>
         <button className="icon-btn" onClick={() => setShowInfo(!showInfo)}>
